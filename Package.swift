9882c8210f0e10d9e7aeedc6d492458d67780dfe
Mihael Isaev <isaev.mihael@gmail.com>
2020-01-07 03:10:10 +0400

    Set minimum SwifQL version to 1.3.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "SwifQLNIO", targets: ["SwifQLNIO"]),
         ],
     dependencies: [
-        .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from:"1.0.0"),
+        .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from:"1.3.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.10.0"),
         ],
     targets: [
